3121e3bb34d1940e0dd2655b7140c7a5a2f89e2b
pengyanb <pengyanbing@msn.com>
2015-10-28 11:52:14 +1300

    Initial Commit

---
+++ NetworkUtilityToolsUITests/NetworkUtilityToolsUITests.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkUtilityToolsUITests.swift
+//  NetworkUtilityToolsUITests
+//
+//  Created by Yanbing Peng on 28/10/15.
+//  Copyright © 2015 Yanbing Peng. All rights reserved.
+//
+
+import XCTest
+
+class NetworkUtilityToolsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
